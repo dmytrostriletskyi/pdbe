dd7559dfbb27984829095955776f97061219a3c2
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-02-02 01:12:19 +0200

    I-4: import pdb statement under commented functions

--- pdbe/pdbe.py
+++ pdbe/pdbe.py
@@ -27,6 +27,13 @@ def does_line_contains_import_pdb(line: str) -> bool:
     return 'import pdb; pdb.set_trace()' in line
 
 
+def is_commended_function(line: str) -> bool:
+    """
+    Check if function is commended.
+    """
+    return line.strip()[0] == '#'
+
+
 def get_function_indent(line: str) -> int:
     """
     Get function indents from begging of the file.
@@ -66,7 +73,7 @@ def put_import_pdb(file_path: str) -> None:
     with fdopen(fh, 'w') as new_file:
         with open(file_path) as old_file:
             for line in old_file:
-                if is_function_sign_in_line(line):
+                if is_function_sign_in_line(line) and not is_commended_function(line):
                     indents_space_count = get_function_indent(line)
                     import_pdb_line_begging_spaces = get_import_pdb_line_st_spaces(indents_space_count)
                     new_file.write(line + import_pdb_line_begging_spaces + IMPORT_PDB_LINE)
