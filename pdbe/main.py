5cf0ffc9ab37923f2153ad2e58ff2ccf704a40bc
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-02-11 01:14:13 +0200

    Issue-12: make multiple func declaration support

--- pdbe/main.py
+++ pdbe/main.py
@@ -21,13 +21,33 @@ def put_import_pdb(file_path: str) -> None:
 
     with fdopen(fh, 'w') as new_file:
         with open(file_path) as old_file:
+
+            # for multiple function declaration
+            import_pdb_statement_to_write = ''
+            multiple_function_declaration_begging_spaces = ''
+
             for line in old_file:
-                if utils.is_function_sign_in_line(line) and not utils.is_commended_function(line):
+                if len(multiple_function_declaration_begging_spaces) > 0:
+                    if 'def' not in line and '):' in line:
+                        import_pdb_line_begging_spaces = multiple_function_declaration_begging_spaces
+                        import_pdb_statement_to_write = import_pdb_line_begging_spaces + utils.IMPORT_PDB_LINE
+                        multiple_function_declaration_begging_spaces = ''
+
+                if utils.is_one_line_function_declaration_line(line) and not utils.is_commended_function(line):
                     indents_space_count = utils.get_function_indent(line)
                     import_pdb_line_begging_spaces = utils.get_import_pdb_line_st_spaces(indents_space_count)
                     new_file.write(line + import_pdb_line_begging_spaces + utils.IMPORT_PDB_LINE)
+
                 else:
+                    if 'def' in line and not utils.is_commended_function(line):
+                        indents_space_count = utils.get_function_indent(line)
+                        multiple_function_declaration_begging_spaces = utils.get_import_pdb_line_st_spaces(indents_space_count)
+
                     new_file.write(line)
+
+                if import_pdb_statement_to_write:
+                    new_file.write(import_pdb_statement_to_write)
+                    import_pdb_statement_to_write = ''
 
     utils.change_files_data(file_path, abs_path)
 
